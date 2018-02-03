5ab71c97eb55110716c5e1190a4c2a3266955a86
Jordan Small <jsmall53@ymail.com>
2018-02-02 21:08:17 -0800

    Added bootstrapper

--- VirtualPlayer/App.xaml.cs
+++ VirtualPlayer/App.xaml.cs
@@ -10,6 +10,14 @@ namespace VirtualPlayer {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application {
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
     }
 }
